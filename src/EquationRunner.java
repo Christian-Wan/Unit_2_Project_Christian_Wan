9218837b8a07680b44b69c819219f98318d303b1
Christian Wan <christian.wan295@gmail.com>
2023-10-04 13:58:13 -0400

    added the rest of the methods in the Design Document and wrote the runner file. Added a welcome and instructions to runner and changed slopeCalculate and distanceCalculate to have one of the variables to be typecasted to double

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
+
 public class EquationRunner {
+    public static void main(String[] args) {
+        //makes scanner
+        Scanner s = new Scanner(System.in);
+
+        //variables
+        String coordinateOne; //(will store the first coordinate pair given by the user)
+        String coordinateTwo; //(will store the second coordinate pair given by the user)
+        int xThree; //(will store the x coordinate given by the user)
+
+
+        System.out.println("Welcome to the linear equation calculator" + //gives welcome and instructions
+                         "\nPlease type out coordinates as (x,y)" +
+                         "\nPlease only use integers");
+        //Gets user input for coordinates
+        System.out.print("What is your first coordinate: ");
+        coordinateOne = s.nextLine();
+        System.out.print("What is your second coordinate: ");
+        coordinateTwo = s.nextLine();
+
+        //creates object and prints out info
+        LinearEquation line = new LinearEquation(coordinateOne, coordinateTwo);
+        System.out.println(line);
+
+        //Gets third x
+        System.out.print("Enter a third x-value: ");
+        xThree = Integer.parseInt(s.nextLine());
+
+        //print out the third coordinate pair
+        System.out.println("Solved coordinate point is: " + line.solvePoint(xThree));
+    }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,28 +1,55 @@
 public class LinearEquation {
     //Instance Variables
-    private int xOne;
-    private int yOne;
-    private int xTwo;
-    private int yTwo;
-    private int xThree;
+    private int xOne; //(Will store the value of the first x value)
+    private int yOne; //(Will store the value of the first y value)
+    private int xTwo; //(Will store the value of the second x value)
+    private int yTwo; //(Will store the value of the second y value)
 
 
     //Constructor
-    public LinearEquation(String coordinateOne, String coordinateTwo) {
-
+    public LinearEquation(String coordinateOne, String coordinateTwo) { //(stores coordinates to instance variables)
+        String firstX = coordinateOne.substring(1, coordinateOne.indexOf(","));
+        String firstY = coordinateOne.substring(coordinateOne.indexOf(",") + 1, coordinateOne.length() - 1);
+        String secondX = coordinateTwo.substring(1, coordinateTwo.indexOf(","));
+        String secondY = coordinateTwo.substring(coordinateTwo.indexOf(",") + 1, coordinateTwo.length() - 1);
+        xOne = Integer.parseInt(firstX);
+        yOne = Integer.parseInt(firstY);
+        xTwo = Integer.parseInt(secondX);
+        yTwo = Integer.parseInt(secondY);
     }
     //Methods
-    public double slopeCalculate() {
-        double slope = (yTwo - yOne) / (xTwo - xOne);
+    public double slopeCalculate() { //(Will find the slope of the line)
+        double slope = Math.round((((double) yTwo - yOne) / (xTwo - xOne)) * 100.00) / 100.00;
         return slope;
     }
 
-    public double yInterceptCalculate() {
-        double intercept = yOne - (slopeCalculate() * xOne);
+    public double yInterceptCalculate() { //(Will find the y-intercept of the line)
+        double intercept = Math.round((yOne - (slopeCalculate() * xOne)) * 100.00) / 100.00;
         return intercept;
     }
 
-    public String equationForm() {
-        String equation =
+    public String equationForm() { //(Will find the slope intercept form)
+        String equation = "y = " + (yTwo - yOne) + "/" + (xTwo - xOne) + "x + " + yInterceptCalculate();
+        return equation;
+    }
+
+    public double distanceCalculate() { //(Finds the distance between the two points)
+        double distance = Math.round(Math.sqrt(Math.pow(((double) xTwo - xOne), 2) + Math.pow(((double) yTwo - yOne), 2)) * 100.00) / 100.00;
+        return distance;
+    }
+
+    public String solvePoint(int xThree) { //(Will find the coordinate point of specified x value)
+        String coordinate = "(" + (double) xThree + ", " + (xThree * slopeCalculate() + yInterceptCalculate()) + ")";
+        return coordinate;
+    }
+
+    public String toString() { //(Will return both coordinate pairs, slope, y-intercept, slope intercept form, difference between points)
+        String result = "First pair: (" + xOne + "," + yOne + ")" +
+                      "\nSecond pair: (" + xTwo + "," + yTwo + ")" +
+                      "\nSlope of line: " + slopeCalculate() +
+                      "\nY-intercept: " + yInterceptCalculate() +
+                      "\nSlope intercept form: " + equationForm() +
+                      "\nDistance between points: " + distanceCalculate();
+        return result;
     }
 }
